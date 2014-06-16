acc9be37da5501c151f60f78ca5bd86bf14276b1
Toby Smithe <git@tsmithe.net>
2014-06-16 11:54:31 +0100

    Add AMG and SPAI preconditioner tags

--- src/_viennacl/preconditioners.cpp
+++ src/_viennacl/preconditioners.cpp
@@ -4,7 +4,7 @@ PYVCL_SUBMODULE(preconditioners)
 {
 
   /*
-
+  
     TODO:
     + preconditioners themselves
       - do this in .hpp and export for the various sparse types
@@ -13,17 +13,21 @@ PYVCL_SUBMODULE(preconditioners)
       - block_ilu_precond (check docs for args; ilu0/ilut)
       - jacobi_precond
       - row_scaling_precond
+      - amg_precond
+      - (f)spai_precond
     + add preconditioner support to solver calls
 
    */
 
+  // [BLOCK-]ILU(T/0)
+
   DISAMBIGUATE_CLASS_FUNCTION_PTR(vcl::linalg::ilut_tag, bool,
                                   use_level_scheduling,
-                                  ilut_get_level_scheduling, () const)
+                                  ilut_get_level_scheduling, () const);
   DISAMBIGUATE_CLASS_FUNCTION_PTR(vcl::linalg::ilut_tag, void,
                                   use_level_scheduling,
-                                  ilut_set_level_scheduling, (bool))
-    bp::class_<vcl::linalg::ilut_tag>("ilut_tag")
+                                  ilut_set_level_scheduling, (bool));
+  bp::class_<vcl::linalg::ilut_tag>("ilut_tag")
     .def(bp::init<unsigned int, double, bool>())
     .add_property("entries_per_row",
                   &vcl::linalg::ilut_tag::get_entries_per_row,
@@ -38,22 +42,105 @@ PYVCL_SUBMODULE(preconditioners)
 
   DISAMBIGUATE_CLASS_FUNCTION_PTR(vcl::linalg::ilu0_tag, bool,
                                   use_level_scheduling,
-                                  ilu0_get_level_scheduling, () const)
+                                  ilu0_get_level_scheduling, () const);
   DISAMBIGUATE_CLASS_FUNCTION_PTR(vcl::linalg::ilu0_tag, void,
                                   use_level_scheduling,
-                                  ilu0_set_level_scheduling, (bool))
-    bp::class_<vcl::linalg::ilu0_tag>("ilu0_tag")
+                                  ilu0_set_level_scheduling, (bool));
+  bp::class_<vcl::linalg::ilu0_tag>("ilu0_tag")
     .def(bp::init<bool>())
     .add_property("use_level_scheduling",
                   ilu0_get_level_scheduling,
                   ilu0_set_level_scheduling)
     ;
   
+  // JACOBI
+
   bp::class_<vcl::linalg::jacobi_tag>("jacobi_tag");
+
+  // ROW SCALING
 
   bp::class_<vcl::linalg::row_scaling_tag>("row_scaling_tag")
     .def(bp::init<unsigned int>())
     .add_property("norm", &vcl::linalg::row_scaling_tag::norm)
     ;
 
+  // AMG
+
+  bp::scope().attr("VIENNACL_AMG_COARSE_RS") = VIENNACL_AMG_COARSE_RS;
+  bp::scope().attr("VIENNACL_AMG_COARSE_ONEPASS") = VIENNACL_AMG_COARSE_ONEPASS;
+  bp::scope().attr("VIENNACL_AMG_COARSE_RS0") = VIENNACL_AMG_COARSE_RS0;
+  bp::scope().attr("VIENNACL_AMG_COARSE_RS3") = VIENNACL_AMG_COARSE_RS3;
+  bp::scope().attr("VIENNACL_AMG_COARSE_AG") = VIENNACL_AMG_COARSE_AG;
+
+  bp::scope().attr("VIENNACL_AMG_INTERPOL_DIRECT") = VIENNACL_AMG_INTERPOL_DIRECT;
+  bp::scope().attr("VIENNACL_AMG_INTERPOL_CLASSIC") = VIENNACL_AMG_INTERPOL_CLASSIC;
+  bp::scope().attr("VIENNACL_AMG_INTERPOL_AG") = VIENNACL_AMG_INTERPOL_AG;
+  bp::scope().attr("VIENNACL_AMG_INTERPOL_SA") = VIENNACL_AMG_INTERPOL_SA;
+
+  bp::class_<vcl::linalg::amg_tag>("amg_tag")
+    .def(bp::init<unsigned int, unsigned int,
+         double, double, double, 
+         unsigned int, unsigned int, unsigned int>())
+    .add_property("coarse",
+                  &vcl::linalg::amg_tag::get_coarse,
+                  &vcl::linalg::amg_tag::set_coarse)
+    .add_property("interpol",
+                  &vcl::linalg::amg_tag::get_interpol,
+                  &vcl::linalg::amg_tag::set_interpol)
+    .add_property("threshold",
+                  &vcl::linalg::amg_tag::get_threshold,
+                  &vcl::linalg::amg_tag::set_threshold)
+    .add_property("jacobiweight",
+                  &vcl::linalg::amg_tag::get_jacobiweight,
+                  &vcl::linalg::amg_tag::set_as)
+    .add_property("interpolweight",
+                  &vcl::linalg::amg_tag::get_interpolweight,
+                  &vcl::linalg::amg_tag::set_interpolweight)
+    .add_property("presmooth",
+                  &vcl::linalg::amg_tag::get_presmooth,
+                  &vcl::linalg::amg_tag::set_presmooth)
+    .add_property("postsmooth",
+                  &vcl::linalg::amg_tag::get_postsmooth,
+                  &vcl::linalg::amg_tag::set_postsmooth)
+    .add_property("coarselevels",
+                  &vcl::linalg::amg_tag::get_coarselevels,
+                  &vcl::linalg::amg_tag::set_coarselevels)
+    ;
+
+  // (F)SPAI
+
+  bp::class_<vcl::linalg::spai_tag>("spai_tag")
+    .def(bp::init<double, unsigned int, double, bool, bool>())
+    .add_property("residual_norm_threshold",
+                  &vcl::linalg::spai_tag::getResidualNormThreshold,
+                  &vcl::linalg::spai_tag::setResidualNormThreshold)
+    .add_property("iteration_limit",
+                  &vcl::linalg::spai_tag::getIterationLimit,
+                  &vcl::linalg::spai_tag::setIterationLimit)
+    .add_property("residual_threshold",
+                  &vcl::linalg::spai_tag::getResidualThreshold,
+                  &vcl::linalg::spai_tag::setResidualThreshold)
+    .add_property("is_static",
+                  &vcl::linalg::spai_tag::getIsStatic,
+                  &vcl::linalg::spai_tag::setIsStatic)
+    .add_property("is_right",
+                  &vcl::linalg::spai_tag::getIsRight,
+                  &vcl::linalg::spai_tag::setIsRight)
+    ;
+
+  bp::class_<vcl::linalg::fspai_tag>("fspai_tag")
+    .def(bp::init<double, unsigned int, bool, bool>())
+    .add_property("residual_norm_threshold",
+                  &vcl::linalg::fspai_tag::getResidualNormThreshold,
+                  &vcl::linalg::fspai_tag::setResidualNormThreshold)
+    .add_property("iteration_limit",
+                  &vcl::linalg::fspai_tag::getIterationLimit,
+                  &vcl::linalg::fspai_tag::setIterationLimit)
+    .add_property("is_static",
+                  &vcl::linalg::fspai_tag::getIsStatic,
+                  &vcl::linalg::fspai_tag::setIsStatic)
+    .add_property("is_right",
+                  &vcl::linalg::fspai_tag::getIsRight,
+                  &vcl::linalg::fspai_tag::setIsRight)
+    ;
 }
--- src/_viennacl/preconditioners.hpp
+++ src/_viennacl/preconditioners.hpp
@@ -2,8 +2,11 @@
 #define _PYVIENNACL_PRECONDITIONERS_H
 
 #include "viennacl.h"
+
+#include <viennacl/linalg/amg.hpp>
 #include <viennacl/linalg/ilu.hpp>
 #include <viennacl/linalg/jacobi_precond.hpp>
 #include <viennacl/linalg/row_scaling.hpp>
+#include <viennacl/linalg/spai.hpp>
 
 #endif
