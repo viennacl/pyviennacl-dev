72b3c35eacc490689adf54aba5d45c82fe2072ed
Toby Smithe <git@tsmithe.net>
2014-07-24 17:38:52 +0100

    Begin new test suite using pytest; old tests now broken

---
+++ tests/_common.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+import logging, math, os, random
+import numpy as np
+import pyviennacl as p
+
+logger = logging.getLogger('pyviennacl')
+default_context = p.backend.Context()
+
+
+def diff(a, b):
+    ret = 0
+
+    # Convert NumPy types to ViennaCL types (they're much more convenient!)
+    if isinstance(a, np.ndarray):
+        if a.ndim == 1:
+            a = p.Vector(a, dtype = a.dtype)
+        elif a.ndim == 2:
+            if isinstance(b, p.MagicMethods):
+                a = p.Matrix(a, dtype = a.dtype, layout = b.layout)
+            else:
+                a = p.Matrix(a, dtype = a.dtype)
+        else:
+            raise TypeError("Something went wrong")
+
+    if isinstance(b, np.ndarray):
+        if b.ndim == 1:
+            b = p.Vector(b)
+        elif b.ndim == 2:
+            if isinstance(a, p.MagicMethods):
+                b = p.Matrix(b, dtype = b.dtype, layout = a.layout)
+            else:
+                b = p.Matrix(b, dtype = b.dtype)
+        else:
+            raise TypeError("Something went wrong")
+
+    # The MagicMethods class guarantees that we have some useful facilities
+    #   (and both Node and Leaf are derived from MagicMethods)
+    if isinstance(a, p.MagicMethods) and isinstance(b, p.MagicMethods):
+        if a.result_container_type is p.Matrix and b.result_container_type is p.Matrix and a.layout != b.layout:
+            # We want to make sure that a and b have the same layout
+            # So construct a temporary matrix, and assign b to it
+            temp = p.Matrix(b.shape, dtype = b.dtype, layout = a.layout)
+            p.Assign(temp, b.result).execute()
+            b = temp
+        d = p.ElementFabs(a - b)
+        cpu_d = d.as_ndarray()
+        if len(d.shape) == 1:
+            # vector
+            for i in range(d.shape[0]):
+                act = math.fabs(cpu_d[i])
+                if act > ret:
+                    ret = act
+        elif len(d.shape) == 2:
+            # matrix
+            for i in range(d.shape[0]):
+                for j in range(d.shape[1]): 
+                    act = math.fabs(cpu_d[i, j])
+                    if act > ret:
+                        ret = act
+        else:
+            raise TypeError("Something went wrong..")
+        return ret
+    else:
+        # We don't have either ndarrays or ViennaCL types so assume plain scalar
+        return math.fabs(a - b) / max(math.fabs(a), math.fabs(b))
+
+
+def get_host_scalar(dtype):
+    return p.HostScalar(random.random(), dtype=dtype)
+
+
+def get_device_scalar(dtype):
+    return p.Scalar(random.random(), dtype=dtype)
+
+
+def get_vector(size, dtype, context=default_context):
+    numpy_A = np.random.rand(size)
+    numpy_A = np.asarray(numpy_A, dtype=dtype)
+    vcl_A = p.Vector(numpy_A, context=context)
+    return numpy_A, vcl_A
+
+
+def get_vector_range(size, dtype, context=default_context):
+    numpy_A, vcl_A = get_vector(size, dtype, context)
+
+    big_A = np.ones((size*4,), dtype=dtype)
+
+    vcl_big_range_A = p.Vector(big_A, context=context)
+    vcl_big_range_A[size:2*size] = vcl_A
+    vcl_range_A = vcl_big_range_A[size:2*size]
+
+    return numpy_A, vcl_range_A
+
+
+def get_vector_slice(size, dtype, context=default_context):
+    numpy_A, vcl_A = get_vector(size, dtype, context)
+
+    big_A = np.ones((size*4,), dtype=dtype)
+
+    vcl_big_slice_A = p.Vector(big_A, context=context)
+    vcl_big_slice_A[size:-size:2] = vcl_A
+    vcl_slice_A = vcl_big_slice_A[size:-size:2]
+
+    return numpy_A, vcl_slice_A
+
+
+def get_numpy_upper_matrix(size1, size2, order, dtype):
+    numpy_A = np.random.rand(size1, size2)
+    numpy_A = np.asarray(numpy_A, dtype=dtype, order=order)
+    for i in range(size1):
+        for j in range(size2):
+            if j < i:
+                numpy_A[i, j] = 0
+    return numpy_A
+
+
+def get_numpy_unit_upper_matrix(size1, size2, order, dtype):
+    numpy_A = get_numpy_upper_matrix(size1, size2, order=order, dtype=dtype)
+    for i in range(size1):
+        for j in range(size2):
+            if i == j:
+                numpy_A[i, j] = 1.0
+    return numpy_A
+
+
+def get_numpy_lower_matrix(size1, size2, order, dtype):
+    return get_numpy_upper_matrix(size2, size1, order, dtype).T
+
+
+def get_numpy_unit_lower_matrix(size1, size2, order, dtype):
+    return get_numpy_unit_upper_matrix(size2, size1, order, dtype).T
+
+
+numpy_matrix_forms = {
+    'upper': get_numpy_upper_matrix,
+    'lower': get_numpy_lower_matrix,
+    'unit_upper': get_numpy_unit_upper_matrix,
+    'unit_lower': get_numpy_unit_lower_matrix
+}
+
+
+def get_matrix(size1, size2, layout, dtype,
+               form=None, context=default_context):
+    if form is None:
+        numpy_A = np.random.rand(size1,size2)
+        numpy_A = np.asarray(numpy_A, order=layout, dtype=dtype)
+    else:
+        numpy_A = numpy_matrix_forms[form](size1, size2, layout, dtype)
+
+    vcl_A = p.Matrix(numpy_A, layout=layout, context=context)
+    return numpy_A, vcl_A
+
+
+def get_matrix_range(size1, size2, layout, dtype,
+                     form=None, context=default_context):
+    numpy_A, vcl_A = get_matrix(size1, size2, layout, dtype, form, context)
+
+    big_A = np.ones((size1 * 4, size2 * 4), order=layout, dtype=dtype)
+
+    vcl_big_range_A = p.Matrix(big_A, layout=layout, context=context)
+    vcl_big_range_A[size1:2*size1, size2:2*size2] = vcl_A
+    vcl_range_A = vcl_big_range_A[size1:2*size1, size2:2*size2]
+
+    return numpy_A, vcl_range_A
+
+
+def get_matrix_slice(size1, size2, layout, dtype,
+                     form=None, context=default_context):
+    numpy_A, vcl_A = get_matrix(size1, size2, layout, dtype, form, context)
+
+    big_A = np.ones((size1 * 4, size2 * 4), order=layout, dtype=dtype)
+
+    vcl_big_slice_A = p.Matrix(big_A, layout=layout, context=context)
+    vcl_big_slice_A[size1:-size1:2, size2::3] = vcl_A
+    vcl_slice_A = vcl_big_slice_A[size1:-size1:2, size2::3]
+
+    return numpy_A, vcl_slice_A
---
+++ tests/test_matrix_operations.py
@@ -0,0 +1,209 @@
+from __future__ import division
+
+import pyviennacl as p
+import numpy as np
+from _common import *
+from itertools import product
+
+layouts = [p.ROW_MAJOR, p.COL_MAJOR]
+matrix_getters = [get_matrix, get_matrix_range, get_matrix_slice]
+vector_getters = [get_vector, get_vector_range, get_vector_slice]
+scalar_getters = [get_host_scalar, get_device_scalar]
+dtype_tolerances = [(p.float32, 1.0E-3), (p.float64, 1.0E-11)]
+
+def dot(A, B):
+    return A.dot(B)
+
+def mul(A, B):
+    return A * B
+
+def pow(A, B):
+    return A ** B
+
+def div(A, B):
+    return A / B
+
+def floordiv(A, B):
+    return A // B
+
+def add(A, B):
+    return A + B
+
+def iadd(A, B):
+    A += B
+    return A 
+
+def scaled_add_left1(A, B, p):
+    return (p * A) + B
+
+def scaled_add_right1(A, B, p):
+    return A + (B * p)
+
+def scaled_add_both1(A, B, p):
+    return (p * A) + (B * p)
+
+def scaled_add_left2(A, B, p):
+    return (A / p) + B
+
+def scaled_add_right2(A, B, p):
+    return A + (B / p)
+
+def scaled_add_both2(A, B, p):
+    return (A / p) + (B / p)
+
+def sub(A, B):
+    return A - B
+
+def isub(A, B):
+    A -= B
+    return A
+
+def scaled_sub_left1(A, B, p):
+    return (p * A) - B
+
+def scaled_sub_right1(A, B, p):
+    return A - (B * p)
+
+def scaled_sub_both1(A, B, p):
+    return (p * A) - (B * p)
+
+def scaled_sub_left2(A, B, p):
+    return (A / p) - B
+
+def scaled_sub_right2(A, B, p):
+    return A - (B / p)
+
+def scaled_sub_both2(A, B, p):
+    return (A / p) - (B / p)
+
+
+A_matrix_operations = [
+#    ('abs', 'np.absolute', 'p.absolute'),
+#    ('acos', 'np.arccos', 'p.arctan'),
+#    ('asin', 'np.arcsin', 'p.arcsin'),
+#    ('atan', 'np.arctan', 'p.arccos'),
+#    ('ceil', 'np.ceil', 'p.ceil'),
+#    ('cos', 'np.cos', 'p.cos'),
+#    ('cosh', 'np.cosh', 'p.cosh'),
+#    ('exp', 'np.exp', 'p.exp'),
+#    ('fabs', 'np.fabs', 'p.fabs'),
+#    ('floor', 'np.floor', 'p.floor'),
+#    ('log', 'np.log', 'p.log'),
+#    ('log10', 'np.log10', 'p.log10'),
+#    ('sin', 'np.sin', 'p.sin'),
+#    ('sinh', 'np.sinh', 'p.sinh'),
+#    ('sqrt', 'np.sqrt', 'p.sqrt'),
+#    ('tan', 'np.tan', 'p.tan'),
+#    ('tanh', 'np.tanh', 'p.tanh')
+]
+
+Ap_matrix_operations = [
+    ('mul', 'mul', 'mul'),
+    ('div', 'div', 'div')
+]
+
+Ax_matrix_operations = [
+    ('dot_vector', 'dot', 'dot')
+]
+
+AB_matrix_operations = [
+    ('add', 'add', 'add'),
+    ('iadd', 'iadd', 'iadd'),
+    ('sub', 'sub', 'sub'),
+    ('isub', 'isub', 'isub'),
+
+    #('elementwise_pow', 'pow', 'pow'),
+    ('elementwise_prod', 'mul', 'p.ElementProd'),
+    ('elementwise_div', 'div', 'p.ElementDiv')
+ ]
+
+ABp_matrix_operations = [
+    ('scaled_add_left1', 'scaled_add_left1', 'scaled_add_left1'),
+    ('scaled_add_right1', 'scaled_add_right1', 'scaled_add_right1'),
+    ('scaled_add_both1', 'scaled_add_both1', 'scaled_add_both1'),
+
+    ('scaled_sub_left1', 'scaled_sub_left1', 'scaled_sub_left1'),
+    ('scaled_sub_right1', 'scaled_sub_right1', 'scaled_sub_right1'),
+    ('scaled_sub_both1', 'scaled_sub_both1', 'scaled_sub_both1'),
+
+    ('scaled_add_left2', 'scaled_add_left2', 'scaled_add_left2'),
+    ('scaled_add_right2', 'scaled_add_right2', 'scaled_add_right2'),
+    ('scaled_add_both2', 'scaled_add_both2', 'scaled_add_both2'),
+
+    ('scaled_sub_left2', 'scaled_sub_left2', 'scaled_sub_left2'),
+    ('scaled_sub_right2', 'scaled_sub_right2', 'scaled_sub_right2'),
+    ('scaled_sub_both2', 'scaled_sub_both2', 'scaled_sub_both2')
+]
+
+
+test_code_header = """
+def test_%s_%s_%s():
+    size1, size2 = 11, 10
+    alpha1 = p.Scalar
+"""
+
+test_code_footer = """
+        act_diff = math.fabs(diff(numpy_C, vcl_C))
+        assert act_diff <= tol, "diff was {} > tolerance {}".format(act_diff, tol)
+"""
+
+for layout, d_t in product(layouts, dtype_tolerances):
+    dt = d_t[0]
+    tol = d_t[1]
+
+    for op in A_matrix_operations:
+        test_code = (test_code_header + """
+    for getter1 in matrix_getters:
+        numpy_A, vcl_A = getter1(size1, size2, layout, dt, None)
+        numpy_C = %s(numpy_A)
+        vcl_C = %s(vcl_A)
+""" + test_code_footer) % (op[0], layout, dt.__name__, op[1], op[2])
+        exec(test_code)
+
+    for op in Ap_matrix_operations:
+        test_code = (test_code_header + """
+    for getter1, getter2 in product(matrix_getters, scalar_getters):
+        numpy_A, vcl_A = getter1(size1, size2, layout, dt, None)
+        alpha = getter2(dt)
+        numpy_C = %s(numpy_A, alpha.value)
+        vcl_C = %s(vcl_A, alpha)
+""" + test_code_footer) % (op[0], layout, dt.__name__, op[1], op[2])
+        exec(test_code)
+
+    for op in Ax_matrix_operations:
+        test_code = (test_code_header + """
+    for getter1, getter2 in product(matrix_getters, vector_getters):
+        numpy_A, vcl_A = getter1(size1, size2, layout, dt, None)
+        numpy_x, vcl_x = getter2(size2, dt)
+        numpy_C = %s(numpy_A, numpy_x)
+        vcl_C = %s(vcl_A, vcl_x)
+""" + test_code_footer) % (op[0], layout, dt.__name__, op[1], op[2])
+        exec(test_code)
+
+
+    for op in AB_matrix_operations:
+        test_code = (test_code_header + """
+    for getter1, getter2 in product(matrix_getters, matrix_getters):
+        numpy_A, vcl_A = getter1(size1, size2, layout, dt, None)
+        numpy_B, vcl_B = getter2(size1, size2, layout, dt, None)
+        numpy_C = %s(numpy_A, numpy_B)
+        vcl_C = %s(vcl_A, vcl_B)
+""" + test_code_footer) % (op[0], layout, dt.__name__, op[1], op[2])
+        exec(test_code)
+
+
+    for op in ABp_matrix_operations:
+        test_code = (test_code_header + """
+    for getter1, getter2, getter3 in product(matrix_getters, matrix_getters, scalar_getters):
+        numpy_A, vcl_A = getter1(size1, size2, layout, dt, None)
+        numpy_B, vcl_B = getter2(size1, size2, layout, dt, None)
+        alpha = getter3(dt)
+        numpy_C = %s(numpy_A, numpy_B, alpha.value)
+        vcl_C = %s(vcl_A, vcl_B, alpha)
+""" + test_code_footer) % (op[0], layout, dt.__name__, op[1], op[2])
+        exec(test_code)
+
+
+# TODO
+# + initialisers: scalars, ndarray, Matrix
+# + non-square matrices, trans matrices
