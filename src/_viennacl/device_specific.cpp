c100d3c1a41926842a1698b5937488fe4477229c
Philippe Tillet <phil.tillet@gmail.com>
2014-08-16 05:58:27 +0200

    Device-specific : Added compatibility to the latest core version

--- src/_viennacl/device_specific.cpp
+++ src/_viennacl/device_specific.cpp
@@ -32,15 +32,15 @@ PYVCL_SUBMODULE(device_specific)
     #undef __PROP
   }
   
-  #define WRAP_TEMPLATE(name, ...) bp::scope outer = bp::class_<ds::name, bp::bases<ds::template_base> >(#name, bp::init<ds::name::parameters_type, ## __VA_ARGS__, std::string const &>())
+  #define WRAP_TEMPLATE(name, ...) bp::scope outer = bp::class_<ds::name, bp::bases<ds::template_base> >(#name, bp::init<ds::name::parameters_type, ## __VA_ARGS__>())
   #define WRAP_PARAMETERS(name, ...) bp::class_<ds::name::parameters_type, bp::bases<ds::template_base::parameters_type> >("parameters_type", bp::init< __VA_ARGS__ >())
   #define __PROP_BASE(name, tpname) .def_readonly(#name, &ds::tpname::parameters_type::name)
   //Vector AXPY
   { 
     #define __PROP(name) __PROP_BASE(name, vector_axpy_template)
     WRAP_TEMPLATE(vector_axpy_template);
-    WRAP_PARAMETERS(vector_axpy_template, uint, size_t, size_t, uint)
-        __PROP(num_groups) __PROP(decomposition);
+    WRAP_PARAMETERS(vector_axpy_template, uint, uint, uint, ds::fetching_policy_type)
+        __PROP(num_groups) __PROP(fetching_policy);
     #undef __PROP
   }
   
@@ -48,8 +48,8 @@ PYVCL_SUBMODULE(device_specific)
   { 
     #define __PROP(name) __PROP_BASE(name, matrix_axpy_template)
     WRAP_TEMPLATE(matrix_axpy_template);
-    WRAP_PARAMETERS(matrix_axpy_template, uint, size_t, size_t, size_t, size_t, uint)
-        __PROP(num_groups_0) __PROP(num_groups_1)  __PROP(decomposition);
+    WRAP_PARAMETERS(matrix_axpy_template, uint, uint, uint, uint, uint, ds::fetching_policy_type)
+        __PROP(num_groups_0) __PROP(num_groups_1)  __PROP(fetching_policy);
     #undef __PROP
   }
   
@@ -57,8 +57,8 @@ PYVCL_SUBMODULE(device_specific)
   { 
     #define __PROP(name) __PROP_BASE(name, reduction_template)
     WRAP_TEMPLATE(reduction_template);
-    WRAP_PARAMETERS(reduction_template, uint, size_t, size_t, uint)
-        __PROP(num_groups)  __PROP(decomposition);
+    WRAP_PARAMETERS(reduction_template, uint, uint, uint, ds::fetching_policy_type)
+        __PROP(num_groups)  __PROP(fetching_policy);
     #undef __PROP
   }
   
@@ -66,7 +66,7 @@ PYVCL_SUBMODULE(device_specific)
   { 
     #define __PROP(name) __PROP_BASE(name, row_wise_reduction_template)
     WRAP_TEMPLATE(row_wise_reduction_template, char);
-    WRAP_PARAMETERS(row_wise_reduction_template, uint, size_t, size_t, uint)
+    WRAP_PARAMETERS(row_wise_reduction_template, uint, uint, uint, uint, ds::fetching_policy_type)
         __PROP(num_groups_0);
     #undef __PROP
   }
@@ -75,7 +75,7 @@ PYVCL_SUBMODULE(device_specific)
   { 
     #define __PROP(name) __PROP_BASE(name, matrix_product_template)
     WRAP_TEMPLATE(matrix_product_template, char, char);
-    bp::scope b = WRAP_PARAMETERS(matrix_product_template, uint, size_t, uint, size_t, uint, uint, uint, ds::fetching_policy_type, ds::fetching_policy_type, size_t, size_t)
+    bp::scope b = WRAP_PARAMETERS(matrix_product_template, uint, uint, uint, uint, uint, uint, uint, ds::fetching_policy_type, ds::fetching_policy_type, uint, uint)
         __PROP(kL) __PROP(mS) __PROP(kS) __PROP(nS)
         __PROP(A_fetching_policy) __PROP(B_fetching_policy)
         __PROP(local_fetch_0) __PROP(local_fetch_1)
@@ -83,9 +83,9 @@ PYVCL_SUBMODULE(device_specific)
     
     bp::enum_<ds::fetching_policy_type>
       ("FetchingPolicy")
-      ENUM_VALUE(ds, FETCH_LOCAL)
-      ENUM_VALUE(ds, FETCH_GLOBAL_CONTIGUOUS)
-      ENUM_VALUE(ds, FETCH_GLOBAL_STRIDED)
+      ENUM_VALUE(ds, FETCH_FROM_LOCAL)
+      ENUM_VALUE(ds, FETCH_FROM_GLOBAL_CONTIGUOUS)
+      ENUM_VALUE(ds, FETCH_FROM_GLOBAL_STRIDED)
       ;
     #undef __PROP
   }
