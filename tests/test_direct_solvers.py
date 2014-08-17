63a812ebe3149240dcc7424892e2e408e6bb128d
Toby Smithe <git@tsmithe.net>
2014-08-17 19:37:10 +0100

    Direct solvers test now compared A.dot(X) with B, rather than a NumPy comparison; currently, DGEMV is broken in the core, so there are a few spurious failures

--- tests/test_direct_solvers.py
+++ tests/test_direct_solvers.py
@@ -2,9 +2,6 @@ from __future__ import division
 
 import pyviennacl as p
 import numpy as np
-import numpy.linalg as npla
-import scipy.linalg as spla
-import scipy.sparse.linalg as spspla
 from _common import *
 from itertools import product
 
@@ -43,8 +40,7 @@ for layout_, d_t_, form_, getter1_, getter2_ in product(layouts, dtype_tolerance
             numpy_A, vcl_A = getter1(size1, size2, layout, dt, A_form)
             numpy_B, vcl_B = getter2(size1, size2, layout, dt)
             vcl_X = p.solve(vcl_A, vcl_B, forms_tags[A_form])
-            numpy_X = npla.solve(numpy_A, numpy_B)
-            act_diff = math.fabs(diff(numpy_X, vcl_X))
+            act_diff = math.fabs(diff(vcl_A.dot(vcl_X), vcl_B))
             assert act_diff <= tol, "diff was {} > tolerance {}".format(act_diff, tol)
         return _test
         
@@ -60,9 +56,7 @@ for layout_, d_t_, form_, getter1_, getter2_ in product(layouts, dtype_tolerance
             numpy_A, vcl_A = getter1(size1, size2, layout, dt, A_form)
             numpy_b, vcl_b = getter2(size1, dt)
             vcl_x = p.solve(vcl_A, vcl_b, forms_tags[A_form])
-            numpy_x = npla.solve(numpy_A, numpy_b)
-            act_diff = math.fabs(diff(numpy_x, vcl_x))
-            print(getter1.__name__, getter2.__name__, A_form)
+            act_diff = math.fabs(diff(vcl_A.dot(vcl_x), vcl_b))
             assert act_diff <= tol, "diff was {} > tolerance {}".format(act_diff, tol)
         return _test
         
